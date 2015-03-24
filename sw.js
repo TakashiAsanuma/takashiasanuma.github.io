617d988f6ff18c3dd5d249dcbc64325649e4cd4c
Asanuma Takashi <asanumatakashi@zigexn.co.jp>
2015-03-24 14:59:06 +0900

    cache set

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 //service worker
 
 var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = '';
+//var urlsToCache = '';
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
         console.log('Opened cache');
-        return cache.addAll(urlsToCache);
+        //return cache.addAll(urlsToCache);
       })
   );
 });
