2f839902f4a59da80a9deba02d48fc97789554e6
Asanuma Takashi <asanumatakashi@zigexn.co.jp>
2015-03-24 16:42:46 +0900

    set cache

--- sw.js
+++ sw.js
@@ -3,14 +3,17 @@
 importScripts('serviceworker-cache-polyfill.js');
 
 var CACHE_NAME = 'my-site-cache-v1';
-//var urlsToCache = '';
+var urlsToCache = [
+  'zigexnbu_1.jpeg',
+  'zigexnbu_2.jpeg'
+];
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
         console.log('Opened cache');
-        return cache.addAll('zigexnbu_2.jpeg');
+        return cache.addAll(urlsToCache);
       })
   );
 });
