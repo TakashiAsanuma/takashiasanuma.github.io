f265a1a795aff85acfc4f05a8b702d834adceba2
Asanuma Takashi <asanumatakashi@zigexn.co.jp>
2015-03-24 14:26:08 +0900

    cache set

--- sw.js
+++ sw.js
@@ -5,14 +5,3 @@ var urlsToCache = [
   '/zigexnbu_2.jpeg'
 ];
 
-self.addEventListener('install', function(event) {
-  console.log('ServiceWorker.oninstall: ', e);
-  // インストール処理
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
